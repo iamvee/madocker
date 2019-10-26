919361e2ac700d298c6ab03d30264f8adc1b6b51
iamvee <naeini.v@gmail.com>
2019-10-25 22:48:13 -0700

    test

--- ubuntu-ssh/build-machine.sh
+++ ubuntu-ssh/build-machine.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-ssh-keygen -f ./keys/machine_$1_rsa -P "" -q
-ssh-add ./keys/machine_$1_rsa
-cp ./keys/machine_$1_rsa.pub authorized_keys
-docker build -t ubuntu-machine-c$1 .
+export UBUNTU_DOCKER_KEY="./keys/machine_${1}_rsa"
+rm ${UBUNTU_DOCKER_KEY}*
+
+ssh-keygen -f ${UBUNTU_DOCKER_KEY} -P "" 
+ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2022"
+ssh-add ${UBUNTU_DOCKER_KEY}
+
+
+cp ${UBUNTU_DOCKER_KEY}.pub ./keys/authorized_keys
+docker kill test-ubuntu-machine-c$1
+docker container rm test-ubuntu-machine-c$1
+docker image rm f ubuntu-machine-c$1
+docker build -t ubuntu-machine-c$1 . 
 
--- ubuntu-ssh/run-machine.sh
+++ ubuntu-ssh/run-machine.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker run -d -P  --name test-ubuntu-machine-c$1 ubuntu-machine-c$1
+#docker run -d -P -ti    ubuntu-machine-c$1
+
+docker run -d -P -p 2022:22 -ti --name test-ubuntu-machine-c$1 ubuntu-machine-c$1
 
